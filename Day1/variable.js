8940f9f2bc250836c9b544da7184e0e02b45e6e8
musharofahmed1 <musharofahmed11@gmail.com>
2025-05-12 23:12:45 +0600

    Variable and Module 17 Done

--- Day1/variable.js
+++ Day1/variable.js
@@ -1,2 +1,43 @@
+//variable declaration
 var age = 35;
+var num1 = 10;
+var num2= 10;
+age = 25;
 console.log(age)
+
+//string
+var course_name = "JavaScript Learning";
+//var course_name = 'JavaScript Learning';
+console.log(course_name)
+
+//boolean
+var isProgrammer = true;
+var passed = false;
+console.log(isProgrammer)
+
+//variable type check
+console.log(typeof isProgrammer)
+
+//operation
+console.log(num1+num2) // +, -, *, /, %, **, //
+
+//type conversation
+var float1 = parseInt(22.5);
+console.log(float1)
+
+var num3 = parseInt('23'); //can use parseFloat() function
+console.log(num3)
+
+//use of toFixed() function for declare digit after decimal point
+var float4 = 22.3;
+var float5 = 22.3;
+var total = float4 + float5
+console.log(total.toFixed(2))
+
+//null
+let number = null;
+console.log("Type of number is:" ,typeof number);
+
+//isNaN
+
+
