ad763a3c70e075d28b48797d9a352406906d9fda
musharofahmed1 <musharofahmed11@gmail.com>
2025-05-15 19:54:23 +0600

    Loop

---
+++ Day3/forLoop.js
@@ -0,0 +1,27 @@
+// const numbers = [12,13,14,15,16];
+
+// for (const i of numbers){
+//     console.log(i)
+// }
+  
+// for (number in numbers){
+//     console.log(numbers[number])
+// }
+
+//for...in → iterates over the index keys (property names) of the array (0, 1, 2...)
+
+// numbers[number] gives you the value at each index
+
+// Good for objects, not recommended for arrays
+
+//printing 1 to 10 using for loop
+for(let i = 0;i<=10;i++){
+    
+    console.log(i)
+
+    // if(i >=5){
+    //     break;
+    // }
+    
+}
+  
