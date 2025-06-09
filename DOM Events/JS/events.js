b574c0c98978abecafc8056207c96828592aa9ee
musharofahmed1 <musharofahmed11@gmail.com>
2025-06-09 20:23:10 +0600

    DOM Events Final 2 Option Updated

--- DOM Events/JS/events.js
+++ DOM Events/JS/events.js
@@ -21,3 +21,26 @@ document.getElementById('make-goldenrod').addEventListener('click', function(){
     document.body.style.backgroundColor = 'Goldenrod'
 })
 
+//Final option 1
+function handleOnClick(){
+    const handlerStatus = document.getElementById('handler-status')
+    handlerStatus.innerText = 'Handled By Function attached onclick attribute'
+}
+
+//Final option 2
+document.getElementById('event-listener').addEventListener('click',function(){
+    const handlerstatus = document.getElementById('handler-status');
+    handlerstatus.innerText = 'Text Updated By add Event Listener';
+})
+
+
+//option 2 recap
+
+document.getElementById('btn-update').addEventListener('click',function(){
+    const inputField = document.getElementById('input-field');
+    const inputText = inputField.value
+
+    const p = document.getElementById('input-text-display')
+    p.innerText = inputText;
+   inputText.value = '';
+})
